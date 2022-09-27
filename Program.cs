4239d2f8410e1d57fd13cea14a5ac82162a3a818
PedroZef <zefpedro@hotmail.com>
2022-09-27 14:50:52 -0300

    Implementação e Gerações de Migrations no Projeto

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using TrilhaApiDesafio.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
 builder.Services.AddDbContext<OrganizadorContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
